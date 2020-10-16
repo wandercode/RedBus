5c0ae8f96e97f85f8057ca248f378960024a114c
wandercode <syamilsasi4s@gmail.com>
2020-10-16 09:11:33 +0530

    Done ToDoList using GET and POST method.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace redbus.Pages
 {
     public class IndexModel : PageModel
     {
+        public string[] allTask;
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -19,7 +21,14 @@ namespace redbus.Pages
 
         public void OnGet()
         {
-
+            allTask=System.IO.File.ReadAllLines("dotasks.txt");
+        }
+        public void OnPost()
+        {
+           string task=Request.Form["dolist"];
+           System.IO.File.AppendAllText("dotasks.txt",task+Environment.NewLine);
+           allTask=System.IO.File.ReadAllLines("dotasks.txt");
+           
         }
     }
 }
